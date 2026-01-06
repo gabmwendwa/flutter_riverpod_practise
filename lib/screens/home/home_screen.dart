7c0d94b534c01f21dcdff44a0aa7b8ffa366d392
Gabriel Mutisya <gabrielmwendwa@gmail.com>
2026-01-07 01:03:16 +0300

    Creating stateless consumers in the home_screen for providers with read-only state values; Closes #4

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod_practise/providers/products_provider.dart';
 import 'package:flutter_riverpod_practise/shared/cart_icon.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final allProducts = ref.watch(productsProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Garage Sale Products'),
@@ -14,7 +18,7 @@ class HomeScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: GridView.builder(
-          itemCount: 8,
+          itemCount: allProducts.length,
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
             mainAxisSpacing: 20,
@@ -25,6 +29,34 @@ class HomeScreen extends StatelessWidget {
             return Container(
               padding: const EdgeInsets.all(20),
               color: Colors.blueGrey.withAlpha(15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Image.asset(
+                      allProducts[index].image,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    allProducts[index].title,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    '\$${allProducts[index].price}',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey[700],
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
             );
           },
         ),
